f5466319a3977a11853af41d14fc25fa692afdef
cfelixmac <cfelixmac@outlook.com>
2018-08-03 23:36:08 +0800

    update history page

--- lib/display/history.dart
+++ lib/display/history.dart
@@ -1,6 +1,7 @@
 import 'package:event_bus/event_bus.dart';
 import 'package:flutter/material.dart';
 import 'package:gankio/data/history_result.dart';
+import 'package:gankio/display/history_details.dart';
 import 'package:gankio/presenter/history_presenter.dart';
 
 var _bus = EventBus();
@@ -77,38 +78,62 @@ class HistoryListItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    var widgets = List<Widget>();
+    widgets.add(Text(
+      '${item.title}',
+      maxLines: 2,
+      overflow: TextOverflow.ellipsis,
+      style: TextStyle(fontSize: 17.0, color: Colors.black87, fontWeight: FontWeight.w600),
+    ));
+    widgets.add(Text(
+      ' ${item.publishedAt.length > 10 ? item.publishedAt.substring(0, 10) : item.publishedAt}',
+      style: TextStyle(fontSize: 10.0, color: Colors.blueAccent, fontWeight: FontWeight.w500),
+    ));
+    String content = '';
+    for (HistoryArticleSection section in item.sections) {
+      if (section.articles != null) {
+        for (HistoryArticle article in section.articles) {
+          content += article.title;
+          content += ' | ';
+        }
+      }
+    }
+    widgets.add(Container(
+      child: Text(
+        content,
+        maxLines: 8,
+        overflow: TextOverflow.ellipsis,
+        style: TextStyle(
+          fontSize: 13.3,
+          color: Colors.black54,
+        ),
+      ),
+    ));
+    widgets.add(Container(
+      margin: EdgeInsets.only(top: 8.0),
+      child: MaterialButton(
+        onPressed: () {
+          showDialog(
+              context: context,
+              builder: (context) {
+                return HistoryDetailDialog(item);
+              });
+        },
+        color: Colors.blueAccent,
+        child: Text(
+          '详情',
+          style: TextStyle(fontSize: 13.0, color: Colors.white, letterSpacing: 1.0),
+        ),
+      ),
+      alignment: Alignment.topRight,
+    ));
+
     return Card(
       child: Container(
         padding: EdgeInsets.all(10.0),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
-          children: <Widget>[
-            Text(
-              '${item.title}',
-              maxLines: 2,
-              overflow: TextOverflow.ellipsis,
-              style: TextStyle(fontSize: 17.0, color: Colors.black87, fontWeight: FontWeight.w600),
-            ),
-            Text(
-              ' ${item.publishedAt.length > 10 ? item.publishedAt.substring(0, 10) : item.publishedAt}',
-              style: TextStyle(fontSize: 10.0, color: Colors.blueAccent, fontWeight: FontWeight.w500),
-            ),
-            GestureDetector(
-              child: Container(
-                height: 80.0,
-                color: Colors.red,
-                child: Row(
-                  children: [
-                    Expanded(
-                      child: Text(
-                        'FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF',
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-            ),
-          ],
+          children: widgets,
         ),
       ),
     );
---
+++ lib/display/history_details.dart
@@ -0,0 +1,117 @@
+import 'package:flutter/material.dart';
+import 'package:gankio/data/history_result.dart';
+import 'package:gankio/display/web_view.dart';
+import 'package:gankio/main.dart';
+
+class HistoryDetailDialog extends StatefulWidget {
+  HistoryDetailDialog(this.article);
+
+  final HistoryResultItem article;
+
+  @override
+  _HistoryDetailDialogState createState() => _HistoryDetailDialogState(article);
+}
+
+class _HistoryDetailDialogState extends State<HistoryDetailDialog> {
+  _HistoryDetailDialogState(this._item);
+
+  HistoryResultItem _item;
+
+  @override
+  Widget build(BuildContext context) {
+    var contentWidgets = List<Widget>();
+    for (HistoryArticleSection section in _item.sections) {
+      if (section.articles != null && section.articles.length != 0) {
+        for (HistoryArticle article in section.articles) {
+          contentWidgets.add(_buildItem(section.name, article));
+        }
+      }
+    }
+
+    return SimpleDialog(
+      titlePadding: EdgeInsets.only(top: 15.0, left: 12.0, right: 12.0),
+      contentPadding: EdgeInsets.only(bottom: 12.0),
+      title: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Text(
+            '${(_item.publishedAt != null && _item.publishedAt.length > 10) ? _item.publishedAt.substring(0, 10) : _item
+                .publishedAt}',
+            maxLines: 1,
+            overflow: TextOverflow.ellipsis,
+            style: TextStyle(
+              fontSize: 13.0,
+              color: Colors.orangeAccent,
+              fontWeight: FontWeight.w600,
+            ),
+          ),
+          Text(
+            '${_item.title}',
+            maxLines: 2,
+            overflow: TextOverflow.ellipsis,
+            style: TextStyle(fontSize: 16.0, fontWeight: FontWeight.w600),
+          ),
+        ],
+      ),
+      children: contentWidgets,
+    );
+  }
+
+  Widget _buildItem(String sectionName, HistoryArticle article) {
+    return InkWell(
+      onTap: () {
+        Navigator.push(
+            context,
+            GankNavigator(
+                builder: (context) => WebViewer(
+                      url: article.url,
+                      title: article.title,
+                    )));
+      },
+      child: Container(
+        padding: EdgeInsets.only(left: 12.0, right: 12.0),
+        height: 52.0,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            _buildItemTextWidget(sectionName, article),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Expanded _buildItemTextWidget(String sectionName, HistoryArticle article) {
+    return Expanded(
+      child: Row(
+        children: <Widget>[
+          Expanded(
+            child: RichText(
+              maxLines: 2,
+              overflow: TextOverflow.ellipsis,
+              text: TextSpan(
+                children: <TextSpan>[
+                  TextSpan(
+                    text: '[$sectionName] ',
+                    style: TextStyle(
+                      fontSize: 11.0,
+                      color: Colors.blueAccent,
+                      fontWeight: FontWeight.w500,
+                    ),
+                  ),
+                  TextSpan(
+                    text: '${article.title}',
+                    style: TextStyle(
+                      fontSize: 13.0,
+                      color: Color(0xff464646),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
